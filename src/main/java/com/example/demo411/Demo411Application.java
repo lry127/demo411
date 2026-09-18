02e0a5e3c41151ca48a8fca0a9644a4927c5c124
lry127 <root@shizuka-chan.com>
2026-09-18 17:31:06 +0800

    problem code

--- src/main/java/com/example/demo411/Demo411Application.java
+++ src/main/java/com/example/demo411/Demo411Application.java
@@ -3,6 +3,8 @@ package com.example.demo411;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// import com.example.grpc_test.proto.SimpleGrpc;
+
 @SpringBootApplication
 public class Demo411Application {
 
@@ -11,5 +13,9 @@ public class Demo411Application {
     }
 
 }
+// wont work using spring boot 4.1.1
 
+//class MyImpl extends SimpleGrpc.SimpleImplBase {
+//
+//}
 
